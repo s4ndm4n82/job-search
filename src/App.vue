d72d680027a06f860c2993e4618096bfc3707264
s4ndm4n <sandaruwan12@gmail.com>
2025-02-28 20:43:27 +0530

    End of section 5.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">Hello Vue !!!</h1>
+    <h1 class="mt-8 text-6xl text-center">Hello Vue !!!</h1>
   </div>
 </template>
---
+++ src/playground.js
@@ -0,0 +1,4 @@
+const fruits = ['apple', 'banana', 'orange'];
+const vegetables = ['cucumber', 'radish'];
+
+console.log([...fruits, ...vegetables]);

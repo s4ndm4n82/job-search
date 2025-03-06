9f693f02de17281847f6680b6c63ab446608763d
s4ndm4n <sandaruwan12@gmail.com>
2025-03-06 21:17:52 +0530

    Sync.

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,26 +1,38 @@
 <template>
-  <main>
-    <section class="flex flex-col h-screen bg-yellow-200 pt-10 pb-20">
-      <div class="grid grid-cols-12">
-        <div class="col-span-1 col-start-1 border border-blue-700">
-          1 column
-        </div>
-        <div class="col-span-5 col-start-2 border border-blue-700">
-          5 column
-        </div>
-        <div class="col-span-5 col-start-7 border border-blue-700">
-          5 column
-        </div>
-        <div class="col-span-1 col-start-12 border border-blue-700">
-          1 column
-        </div>
-      </div>
-    </section>
-  </main>
+    <main>
+        <section class="flex flex-col h-screen bg-yellow-200 pt-10 pb-20">
+            <div class="grid grid-cols-12">
+                <div class="col-span-1 col-start-1 border border-blue-700">
+                    1 column
+                </div>
+                <div class="col-span-5 col-start-2 border border-blue-700">
+                    5 column
+                </div>
+                <div class="col-span-5 col-start-7 border border-blue-700">
+                    5 column
+                </div>
+                <div class="col-span-1 col-start-12 border border-blue-700">
+                    1 column
+                </div>
+            </div>
+        </section>
+    </main>
 </template>
 
 <script>
 export default {
-  name: "TheHero",
-};
+    name: 'TheHero',
+    beforeCreate() {
+        console.log('beforeCreate')
+    },
+    created() {
+        console.log('created')
+    },
+    beforeMount() {
+        console.log('beforeMount')
+    },
+    mounted() {
+        console.log('mounted')
+    },
+}
 </script>
--- src/components/TheSubnav.vue
+++ src/components/TheSubnav.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="w-full h-16 border-b border-solid border-brand-gray-1 bg-white">
-    <div class="flex items-center h-full px-8">
-      <div v-if="onJobResultsPage">
-        <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
-        <span><span class="text-brand-green-1">1653</span> jobs matched</span>
-      </div>
+    <div class="w-full h-16 border-b border-solid border-brand-gray-1 bg-white">
+        <div class="flex items-center h-full px-8">
+            <div v-if="onJobResultsPage">
+                <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
+                <span
+                    ><span class="text-brand-green-1">1653</span> jobs
+                    matched</span
+                >
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "TheSubnav",
-  data() {
-    return {
-      onJobResultsPage: true,
-    };
-  },
-};
+    name: 'TheSubnav',
+    data() {
+        return {
+            onJobResultsPage: true,
+        }
+    },
+}
 </script>

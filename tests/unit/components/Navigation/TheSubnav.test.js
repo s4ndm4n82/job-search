8a0eac8a466ed013b533b36db4b3d9382777287a
s4ndm4n <sandaruwan12@gmail.com>
2025-03-11 21:17:24 +0530

    Sync.

--- tests/unit/components/Navigation/TheSubnav.test.js
+++ tests/unit/components/Navigation/TheSubnav.test.js
@@ -3,26 +3,29 @@ import { render, screen } from '@testing-library/vue';
 import TheSubnav from '@/components/Navigation/TheSubnav.vue';
 
 describe('TheSubnav', () => {
+    const renderTheSubnav = (routeName) => {
+        render(TheSubnav, {
+            global: {
+                mocks: {
+                    $route: {
+                        name: routeName,
+                    },
+                },
+                stubs: {
+                    FontAwesomeIcon: true,
+                },
+            },
+            data() {
+                return {
+                    onJobResultsPage: true,
+                };
+            },
+        });
+    };
     describe('when user is on jobs page', () => {
         it('displays job count', () => {
-            const $route = {
-                name: 'JosResults',
-            };
-            render(TheSubnav, {
-                global: {
-                    mocks: {
-                        $route,
-                    },
-                    stubs: {
-                        FontAwesomeIcon: true,
-                    },
-                },
-                data() {
-                    return {
-                        onJobResultsPage: true,
-                    };
-                },
-            });
+            const routeName = 'JosResults';
+            renderTheSubnav(routeName);
 
             const jobCount = screen.getByText('1653');
 
@@ -32,24 +35,8 @@ describe('TheSubnav', () => {
 
     describe('when user is not on jobs page', () => {
         it('does NOT display job count', () => {
-            const $route = {
-                name: 'Home',
-            };
-            render(TheSubnav, {
-                global: {
-                    mocks: {
-                        $route,
-                    },
-                    stubs: {
-                        FontAwesomeIcon: true,
-                    },
-                },
-                data() {
-                    return {
-                        onJobResultsPage: false,
-                    };
-                },
-            });
+            const routeName = 'Home';
+            renderTheSubnav(routeName);
 
             const jobCount = screen.queryByText('1653');
 

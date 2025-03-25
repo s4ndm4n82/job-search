07ce69bfcdb823da0e8b57ec264fa5b121984dd4
s4ndm4n <sandaruwan12@gmail.com>
2025-03-25 21:24:10 +0530

    Sync.

---
+++ src/views/TeamsView.vue
@@ -0,0 +1,23 @@
+<template>
+  <header-container>
+    <template #title>
+      <h1 class="w-full text-4xl font-normal">Teams</h1>
+    </template>
+    <template #subtitle>
+      <p class="w-full text-lg font-light">
+        Find the perfect team for your next project
+      </p>
+    </template>
+  </header-container>
+</template>
+
+<script>
+import HeaderContainer from "@/components/Shared/HeaderContainer.vue";
+
+export default {
+  name: "TeamsView",
+  components: {
+    HeaderContainer,
+  },
+};
+</script>
